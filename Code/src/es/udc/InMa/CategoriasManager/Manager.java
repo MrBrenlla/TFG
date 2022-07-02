7bfc586e5461edec70cfbdf17a473a7fc32e0a16
Brais <bgbrenlla@gmail.com>
2022-07-02 18:06:23 +0200

    Arranxado categorías con tilde

--- Code/src/es/udc/InMa/CategoriasManager/Manager.java
+++ Code/src/es/udc/InMa/CategoriasManager/Manager.java
@@ -1,23 +1,25 @@
 package es.udc.InMa.CategoriasManager;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import es.udc.InMa.service.Informacion;
 import es.udc.InMa.service.MyProperties;
+import org.apache.commons.text.StringEscapeUtils;
 
 public class Manager {
 	
 	private static Manager m = null;
 	private Map<String,Categoria> categorias =null;
-	private String[] cat = MyProperties.getProperties().getProperty("CATEGORIAS").toUpperCase().split(";");
+	private String[] cat = MyProperties.getProperties().getProperty("CATEGORIAS").split(";");
 	
 	private Manager() {
 		if (categorias==null) {
 			categorias=new HashMap<String,Categoria>();
 			for (String s : cat) {
-				categorias.putIfAbsent(s, new Categoria());
+				categorias.putIfAbsent(fix(s).toUpperCase(), new Categoria());
 			}
 			categorias.putIfAbsent("OUTRAS", new Categoria());
 		}
@@ -28,9 +30,20 @@ public class Manager {
 		return m;
 	}
 	
+	private String fix(String s) {
+		if(s!=null) {
+			System.out.println(s);
+			s = new String(s.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+			System.out.println(s);
+			s=StringEscapeUtils.escapeHtml4(s.toUpperCase());
+			System.out.println(s);
+		}
+		return s;
+	}
+	
 	//Engade a información á categoría correspondente ou a "OUTRAS" se a cetegoría non existe
 	public void add(Informacion info) {
-		Categoria c = categorias.get(info.getCategoria().toUpperCase());
+		Categoria c = categorias.get(StringEscapeUtils.escapeHtml4(info.getCategoria().toUpperCase()).toUpperCase());
 		if(c==null) c = categorias.get("OUTRAS");
 		c.add(info);
 	}
@@ -47,6 +60,7 @@ public class Manager {
 		}
 		if(l.size()>0) aux=aux+l.get(l.size()-1).toJson()+"\n]";
 		else aux=aux+"]";
+		System.out.println(aux);
 		return aux;
 	}
 	
@@ -56,7 +70,7 @@ public class Manager {
 		ArrayList<String> l=new ArrayList<>();
 		for(String c:cat) {
 			if(!l.contains(c) && !c.equals("OUTRAS")) {
-				aux=aux+"\""+c+"\",\n";
+				aux=aux+"\""+fix(c)+"\",\n";
 				l.add(c);
 			}
 		}
